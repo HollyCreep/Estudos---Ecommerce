fbc69a1a71e79dc242d990e0020f8eb2afa4d837
Lucas Ferreira <lucasferreirarm@gmail.com>
2020-05-05 17:18:44 -0200

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,56 @@
+<template>
+  <v-app id="inspire">
+    <v-navigation-drawer
+      v-model="drawer"
+      app
+      clipped
+      dark
+      src="https://cdn.vuetifyjs.com/images/backgrounds/bg-2.jpg"
+    >
+      <v-list>
+        <v-list-item v-for="(item, i) in destinations" :key="i" link>
+          <v-list-item-icon>
+            <v-icon>{{ item.name }}</v-icon>
+          </v-list-item-icon>
+          <v-icon large>mdi-code-tags</v-icon>
+          <v-list-item-content>
+            <v-list-item-title>info</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
+    </v-navigation-drawer>
+
+    <v-app-bar
+      app
+      src="https://cdn.vuetifyjs.com/images/backgrounds/bg-2.jpg"
+      dark
+      clipped-left
+    >
+      <v-app-bar-nav-icon @click.stop="drawer = !drawer" />
+      <v-toolbar-title>Menu</v-toolbar-title>
+    </v-app-bar>
+
+    <v-content>
+      <router-view></router-view>
+    </v-content>
+    <v-footer color="blue darken-1" app>
+      <span class="white--text">&copy; 2019</span>
+    </v-footer>
+  </v-app>
+</template>
+
+<script>
+import router from "@/router/router.js";
+export default {
+  name: "App",
+  props: {
+    source: String,
+  },
+  data: () => ({
+    drawer: null,
+    destinations: router.menuLateral,
+  }),
+};
+</script>
+
+<style lang="scss"></style>
---
+++ src/views/Home.vue
@@ -0,0 +1,51 @@
+<template>
+  <v-container class="fill-height" fluid>
+    <v-row align-center justify-center>
+      <v-col class="text-center">
+        <h2>Bem vindo ao Mobile Stock!</h2>
+        <v-divider inset></v-divider>
+      </v-col>
+    </v-row>
+    <v-row align="center" justify="center">
+      <v-col class="text-center">
+        <v-tooltip left>
+          <template v-slot:activator="{ on }">
+            <router-link to="about">
+              <v-btn icon large target="_blank" v-on="on">
+                <v-icon large>mdi-code-tags</v-icon>
+              </v-btn>
+            </router-link>
+          </template>
+          <span>Source</span>
+        </v-tooltip>
+
+        <v-tooltip right>
+          <template v-slot:activator="{ on }">
+            <v-btn
+              icon
+              large
+              href="https://codepen.io/johnjleider/pen/zgxeLQ"
+              target="_blank"
+              v-on="on"
+            >
+              <v-icon large>mdi-codepen</v-icon>
+            </v-btn>
+          </template>
+          <span>Codepen</span>
+        </v-tooltip>
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
+
+<script>
+// @ is an alias to /src
+
+export default {
+  name: "Home",
+  components: {},
+  data() {
+    return {};
+  }
+};
+</script>
